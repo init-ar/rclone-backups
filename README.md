3146e035ba1b6b4a42fc137eabcec686372356b3
Juan Larumbe <jularumbe@gmail.com>
2025-05-15 14:21:13 -0300

    Primer version del script

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # rclone-backups
 Script para descargar y empaquetar archivos de la nube
+
+En el script, modificar estas variables:
+
+# Variables de configuración
+LOG_FILE="ruta/al/log"  # Archivo de log
+DESTINATION="/ruta/a/carpeta/local/"   # Carpeta de destino
+
+Para completar estas variables, es neceario ya tener configurado el remote de rclone (Ver documentación oficial para esto)
+
+El comando a usar es:
+
+rclone backend drives init-test:
+
+Devuelve estos datos, usar ID y name según corresponda:
+
+[
+	{
+		"id": "0AIGBJMf6M_XTUk9PVA",
+		"kind": "drive#teamDrive",
+		"name": "INIT"
+	},
+	{
+		"id": "0AHBlLtOeW2ftUk9PVA",
+		"kind": "drive#teamDrive",
+		"name": "Soporte"
+	},
+	{
+		"id": "0ANMk3V85rrRkUk9PVA",
+		"kind": "drive#teamDrive",
+		"name": "SysAdmin"
+	}
+]
+
+
+# Array de Shared Drives con nombres reconocibles
+SHARED_DRIVES=(
+  "Soporte:0AHBlLtOeW2ftUk9PVA"
+  "Sysadmin:0ANMk3V85rrRkUk9PVA"
+)
---
+++ rclone.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Variables de configuración
+LOG_FILE="/home/juan/sync_log.txt"  # Archivo de log
+DESTINATION="/home/juan/init-test/"   # Carpeta de destino
+
+# Array de Shared Drives con nombres reconocibles
+SHARED_DRIVES=(
+  "name-de-drive:ID-del-drive"
+  "name-de-drive2:ID-del-drive2"
+)
+
+# Función para registrar mensajes en el log
+log_message() {
+    local message_type="$1"
+    local message="$2"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') [$message_type] $message" >> "$LOG_FILE"
+}
+
+# Función para sincronizar el drive
+sync_drive() {
+    local drive_info="$1"
+    local drive_name="${drive_info%%:*}"  # Extraer el nombre del drive
+    local drive_id="${drive_info##*:}"      # Extraer el ID del drive
+    local drive_destination="$DESTINATION$drive_name/"  # Carpeta de destino específica para el drive
+
+    # Verificar si la carpeta de destino existe, si no, crearla
+    if [ ! -d "$drive_destination" ]; then
+        mkdir -p "$drive_destination"
+        log_message "INFO" "Carpeta creada: $drive_destination"
+    fi
+
+    log_message "INFO" "Iniciando sincronización para el ID de Drive: $drive_name ($drive_id)"
+    
+    # Ejecutar el comando rclone
+    rclone sync "init-test:" --drive-team-drive "$drive_id" "$drive_destination" --progress --transfers=4 --checkers=8 --drive-acknowledge-abuse
+    
+    # Verificar el estado de la última ejecución
+    if [ $? -eq 0 ]; then
+        log_message "INFO" "Sincronización completada exitosamente para el ID: $drive_name ($drive_id)"
+    else
+        log_message "ERROR" "Error durante la sincronización para el ID: $drive_name ($drive_id)"
+    fi
+}
+
+# Recorrer cada Shared Drive y sincronizar
+for drive_info in "${SHARED_DRIVES[@]}"; do
+    sync_drive "$drive_info"
+done
